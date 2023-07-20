1cef791aefd46051d6818b98eedcce8927afeff0
n.trubanenko <n.trubanenko@evotor.ru>
2023-07-20 22:59:07 +0300

    RegistrationFormTests - correction2

--- src/test/java/registrationForm/RegistrationFormTests.java
+++ src/test/java/registrationForm/RegistrationFormTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import java.io.File;
+
 
 public class RegistrationFormTests {
 
